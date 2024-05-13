9d00be6ae307628a2900594bab1641367554a601
dfeghuijk <gs1reed2829@hotmail.com>
2024-05-13 08:37:01 +0700

    aptos sdk commit

--- index.js
+++ index.js
@@ -1,3 +1,6 @@
-function findNumbers(nums) {
-  return nums.filter((num) => num.toString().length % 2 === 0).length;
-}
+const flattenDepth = (arr, depth = 1) =>
+  arr.reduce(
+    (a, v) =>
+      a.concat(depth > 1 && Array.isArray(v) ? flattenDepth(v, depth - 1) : v),
+    [],
+  );
